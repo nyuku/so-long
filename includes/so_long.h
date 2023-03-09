47e3ccfe103b6c7a68ac31a6775a210f8b4d3c82
Angela Nguyen <angnguye@c3r4s4.42lausanne.ch>
2023-03-09 23:54:37 +0100

    ca marche

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: angnguye <angnguye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 14:55:11 by angnguye          #+#    #+#             */
-/*   Updated: 2023/03/09 20:04:55 by angnguye         ###   ########.fr       */
+/*   Updated: 2023/03/09 22:03:09 by angnguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,20 +20,17 @@
 
 typedef struct s_mlx
 {
-	void *newimage_ptr;
-
-	char *adress;
-	int	bits_per_pixel;
-	int	line_length;
-	int	endian;
+	void	*newimage_ptr;
+	void	*mlx_ptr;
+	void	*mlx_window_ptr;
+	char	*adress;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
 }	t_mlx;
 
-typedef struct s_data
-{
-	void *mlx_ptr;
-	void *win_ptr;
-	t_img	mlx;
-}	t_data;
+
+
 
 
 # endif
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: angnguye <angnguye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/04 17:48:31 by angnguye          #+#    #+#             */
-/*   Updated: 2023/03/09 19:39:41 by angnguye         ###   ########.fr       */
+/*   Updated: 2023/03/09 23:50:45 by angnguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,31 +20,39 @@ int main()
 {
 	t_mlx mlx; // on appelle la structure
 	// printf("Hello World");
-	void *mlx_ptr;
-	void *mlx_window_ptr;
-    char *image_path;//xmp
+
+	
+	// void *mlx_ptr;
+	// void *mlx_window_ptr;
+    char *image_path = "image/blop.xpm";
 	void *img;//xpm
 
-	int	img_width = 1920;//xmp
-	int	img_height = 1080;//xmp
+	int	img_width;//xmp
+	int	img_height;//xmp
 
-	image_path = "../image/coco.xmp";
 
-    mlx_ptr = mlx_init();
-	mlx_window_ptr = mlx_new_window(mlx_ptr, 1920, 1080, "Test :3");
-	mlx.newimage_ptr = mlx_new_image(mlx_ptr, 1920, 1080);
-	mlx.adress = mlx_get_data_addr(mlx.newimage_ptr, &mlx.bits_per_pixel, &mlx.line_length, &mlx.endian);
+    mlx.mlx_ptr = mlx_init();
+	mlx.mlx_window_ptr = mlx_new_window(mlx.mlx_ptr, 750, 600, "Test :3");
 	
 	// my_mlx_pixel_put(&mlx, 5, 5,0x00FF00FF );// met un pixel a 5x5y turquoise
-	img = mlx_xpm_file_to_image(mlx_ptr,image_path, &img_width, img_height);
-
-	mlx_put_image_to_window(mlx_ptr, mlx_window_ptr,mlx.newimage_ptr, 0, 0);
-	mlx_loop(mlx_ptr);// a mettre a la fin pour boucler
+	mlx.newimage_ptr = mlx_xpm_file_to_image(mlx.mlx_ptr, image_path, &img_width, &img_height);
+	if (!mlx.newimage_ptr)
+	{
+		ft_putstr("error file image\n");
+		return(1);
+	}
 	
-	
-	
+	mlx_put_image_to_window(mlx.mlx_ptr, mlx.mlx_window_ptr, mlx.newimage_ptr, 250, 250);
+	mlx_loop(mlx.mlx_ptr);// a mettre a la fin pour boucler
 
 }
+// void	img_pix_put(t_mlx *img, int x, int y, int color)
+// {
+// 	char    *pixel;
+
+//     pixel = mlx->addr + (y * mlx->line_length + x * (mlx->bits_per_pixel / 8));
+// 	*(int *)pixel = color;
+// }
 
 //fonction bricole
 void	my_mlx_pixel_put(t_mlx *mlx, int x, int y, int color)
