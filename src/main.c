270bd54a7c0eafbc469aab01bfbf3f5f17ba9647
Angela Nguyen <angnguye@c1r14s1.42lausanne.ch>
2023-03-11 01:13:55 +0100

    fin de soiree

--- src/main.c
+++ src/main.c
@@ -5,65 +5,102 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: angnguye <angnguye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/04 17:48:31 by angnguye          #+#    #+#             */
-/*   Updated: 2023/03/10 20:21:19 by angnguye         ###   ########.fr       */
+/*   Created: 2023/03/09 14:06:28 by angnguye          #+#    #+#             */
+/*   Updated: 2023/03/11 01:13:39 by angnguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
-
 #include <stdio.h>
+int ft_count_line_map(char *map_sample);
+void print_map(char **map, int line_count);
+char **ft_harvest_map(char *map_sample);
+int	count_colonn(char **str);
 
-void	my_mlx_pixel_put(t_mlx *mlx, int x, int y, int color);
-
-int	key_hook(int keycode, t_mlx *mlx) // notre fonction
+t_map *get_map(int argc, char **argv, t_map map)
 {
-	if (keycode == KEY_UP){
-		mlx.x += 25;
-		// mlx->y += 25;
-		mlx_put_image_to_window(mlx->mlx_ptr, mlx->mlx_window_ptr, mlx->img.newimage_ptr, mlx->x, mlx->y);	
-		printf("Up!\n");
-	}
 	
-	if (keycode == KEY_LFT)
-		printf("Left!\n");
-	if (keycode == KEY_DWN)
-		printf("Down!\n");
-	if (keycode == KEY_RGT)
-		printf("Right!\n");
-	return (0);
+    map.i = 0;
+	map.lines = ft_count_line_map(argv[1]);
+	map.char_map = ft_harvest_map(argv[1]);
+	printf("structure line: %d\n",map.lines);
+	map.colonn = count_colonn(map.char_map);
+	printf("nombre de colonne struc %d\n", map.colonn);
+    // printf("%d\n", map.i);
+	// printf("%s\n, map.char_map");
+
+    // Imprimer la map
+    //print_map(mapu, line_count);
+
+    // Fin du programme
+    ft_printf("End\n");
+
+	return (&map);
 }
 
-int main()
+void print_map(char **map, int line_count) //checker map, print str par str
 {
-	t_mlx mlx; // on appelle la structure mlx
-
-    char *image_path = "image/blop.xpm";
-	// void *img;//xpm
-
-	int	img_width;//xmp
-	int	img_height;//xmp
-
-
-    mlx.mlx_ptr = mlx_init();
-	mlx.mlx_window_ptr = mlx_new_window(mlx.mlx_ptr, WIN_W, WIN_H, "Test :3");
-	
-	mlx.img.newimage_ptr = mlx_xpm_file_to_image(mlx.mlx_ptr, image_path, &img_width, &img_height);
-	if (!mlx.img.newimage_ptr)
-	{
-		ft_putstr("error file image\n");
-		return(ERROR);
-	}
-	mlx.x = 255;
-	mlx.y = 255;
-	while (1)
-	{
-		mlx_put_image_to_window(mlx.mlx_ptr, mlx.mlx_window_ptr, mlx.img.newimage_ptr, mlx.x, mlx.y);	
-		mlx_key_hook(mlx.mlx_window_ptr, key_hook, &mlx);
-		printf("%d\n",mlx.x);
-		printf("%d\n",mlx.y);
-		mlx_loop(mlx.mlx_ptr);// a mettre a la fin pour boucler
-	}
-	
+    int i;
+    i = 0;
+    while (i < line_count)
+    {
+        ft_printf("%s", map[i]);
+        i++;
+    }
 }
 
+int ft_count_line_map(char *map_sample) // compte nombre de ligne dans la map
+{
+	int fd;
+	int count;
+	char *gnl_return;
+
+	count = 0;
+	fd = open(map_sample, O_RDONLY);// on deference pour utiliser map
+	gnl_return = get_next_line(fd);//pour la first, rentrer boucle
+
+	while(gnl_return) // car sera == null a la fin
+	{
+		free(gnl_return); // touche statik, osef on a deja check. propre pour le prochain tour
+		gnl_return = get_next_line(fd);
+		count++;
+	}
+	close (fd);
+	return(count);
+}
+
+char **ft_harvest_map(char *map_sample) // recolte la map en str
+{
+	int fd;
+	int line_map;
+	char **tableau_stock;
+	char *line; //récupérer ce qu'a lu gnl
+	int i; //index tableau
+
+	fd = open(map_sample, O_RDONLY);
+	line_map = ft_count_line_map(map_sample);
+
+	tableau_stock = malloc(sizeof(char *)*(line_map));
+	if (!tableau_stock)
+		return(NULL);
+	while(i < line_map)
+	{
+		line = get_next_line(fd);//faut bien stocker qq part le contenu lu
+		tableau_stock[i] = ft_strdup(line); //duplique la line dans le tableau
+		free (line);//tout ce qui touche gnl...free pour remettre neuf
+        i++;
+	}
+	close (fd);
+	return (tableau_stock);
+}
+
+int	count_colonn(char **str)
+{
+	int i;
+	i = 0;
+	while(str[1][i]!= '\0')
+	{
+		i++;	
+	}
+	return(i-1);
+}
